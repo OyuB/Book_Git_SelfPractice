15a9cab7c32228f654f69cdaf33bac0cb3fa1160
OyuBuren <obmglub@gmail.com>
2023-04-26 08:24:31 -0500

    Chapter One and  Page One created

---
+++ src/Chapter01/page01.java
@@ -0,0 +1,15 @@
+package Chapter01;
+
+public class page01 {
+    public static void main(String[] args) {
+
+        String story ="";
+        story="It all started here.";
+
+        System.out.println(story);
+
+        String line0="My day 01 as a Tester";
+
+        System.out.println(line0);
+    }
+}
